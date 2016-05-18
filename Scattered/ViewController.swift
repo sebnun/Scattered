88858b9fe4d8c4b437c09f2821999cdd99e91473
Sebastian <sebnun@gmail.com>
2016-05-17 22:19:46 -0300

    first commit

---
+++ Scattered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scattered
+//
+//  Created by Sebastian on 5/17/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
