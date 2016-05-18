dbf1a66520b4a58bdd5cc5d6e9ddd74d01d38536
Sebastian <sebnun@gmail.com>
2016-05-18 16:58:52 -0300

    Implemented multithreading for better performance

--- Scattered/ViewController.swift
+++ Scattered/ViewController.swift
@@ -20,59 +20,63 @@ class ViewController: NSViewController {
             size.width = ceil(size.width)
             size.height = ceil(size.height)
             textLayer.bounds = CGRect(origin: CGPoint.zero, size: size)
-            textLayer.superlayer!.bounds
-                = CGRect(x: 0, y: 0, width: size.width + 16, height: size.height + 20)
+            textLayer.superlayer!.bounds = CGRect(x: 0, y: 0, width: size.width + 16, height: size.height + 20)
             textLayer.string = text
         }
     }
     
     
+    let processingQueue: NSOperationQueue = {
+        let result = NSOperationQueue()
+        //result.maxConcurrentOperationCount = 4
+        return result
+    }()
+    
+    
     func addImagesFromFolderURL(folderURL: NSURL) {
-        let t0 = NSDate.timeIntervalSinceReferenceDate()
-        
-        let fileManager = NSFileManager()
-        let directoryEnumerator = fileManager.enumeratorAtURL(folderURL,
-                                                              includingPropertiesForKeys: nil,
-                                                              options: [],
-                                                              errorHandler: nil)!
-        
-        var allowedFiles = 10
-        
-        while let url = directoryEnumerator.nextObject() as? NSURL {
-            // Skip directories:
+        processingQueue.addOperationWithBlock {
+            let t0 = NSDate.timeIntervalSinceReferenceDate()
             
-            var isDirectoryValue: AnyObject?
-            do {
-                try url.getResourceValue(&isDirectoryValue,
-                                         forKey: NSURLIsDirectoryKey)
-            } catch {
-                print("error checking whether URL is directory: \(error)")
-                continue
-            }
+            let fileManager = NSFileManager()
+            let directoryEnumerator = fileManager.enumeratorAtURL(folderURL,
+                includingPropertiesForKeys: nil,
+                options: [],
+                errorHandler: nil)!
             
-            guard let isDirectory = isDirectoryValue as? Bool where
-                isDirectory == false else {
+            while let url = directoryEnumerator.nextObject() as? NSURL {
+                // Skip directories:
+                
+                var isDirectoryValue: AnyObject?
+                do {
+                    try url.getResourceValue(&isDirectoryValue,
+                        forKey: NSURLIsDirectoryKey)
+                } catch {
+                    print("error checking whether URL is directory: \(error)")
                     continue
+                }
+                
+                self.processingQueue.addOperationWithBlock {
+                    guard let isDirectory = isDirectoryValue as? Bool where
+                        isDirectory == false else {
+                            return
+                    }
+                    
+                    guard let image = NSImage(contentsOfURL: url) else {
+                        return
+                    }
+                    
+                    let thumbImage = self.thumbImageFromImage(image)
+                    
+                    NSOperationQueue.mainQueue().addOperationWithBlock {
+                        self.presentImage(thumbImage)
+                        let t1 = NSDate.timeIntervalSinceReferenceDate()
+                        let interval = t1 - t0
+                        self.text = String(format: "%0.1fs", interval)
+                    }
+                }
             }
-            
-            guard let image = NSImage(contentsOfURL: url) else {
-                continue
-            }
-            
-            allowedFiles -= 1
-            guard allowedFiles >= 0 else {
-                break
-            }
-            
-            let thumbImage = thumbImageFromImage(image)
-            
-            presentImage(thumbImage)
-            let t1 = NSDate.timeIntervalSinceReferenceDate()
-            let interval = t1 - t0
-            text = String(format: "%0.1fs", interval)
         }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,6 +87,7 @@ class ViewController: NSViewController {
         
         let textContainer = CALayer()
         textContainer.anchorPoint = CGPoint.zero
+        
         textContainer.position = CGPointMake(10, 10)
         textContainer.zPosition = 100
         textContainer.backgroundColor = NSColor.blackColor().CGColor
@@ -105,8 +110,7 @@ class ViewController: NSViewController {
         // Rely on text's didSet to update textLayer's bounds:
         text = "Loading..."
         
-        let url = NSURL(fileURLWithPath: "/Library/Desktop Pictures",
-                        isDirectory: true)
+        let url = NSURL.fileURLWithPath("/Library/Desktop Pictures", isDirectory: true)
         addImagesFromFolderURL(url)
     }
     
@@ -114,12 +118,10 @@ class ViewController: NSViewController {
     func thumbImageFromImage(image: NSImage) -> NSImage {
         let targetHeight: CGFloat = 200.0
         let imageSize = image.size
-        let smallerSize
-            = NSSize(width: targetHeight * imageSize.width / imageSize.height,
-                     height: targetHeight)
+        let smallerSize = NSSize(width: targetHeight * imageSize.width / imageSize.height, height: targetHeight)
         
         let smallerImage = NSImage(size: smallerSize,
-                                   flipped: false) { rect -> Bool in
+                                   flipped: false) { (rect) -> Bool in
                                     image.drawInRect(rect)
                                     return true
         }
@@ -139,8 +141,7 @@ class ViewController: NSViewController {
             CGPoint(x: CGFloat(arc4random_uniform(UInt32(superlayerBounds.maxX))),
                     y: CGFloat(arc4random_uniform(UInt32(superlayerBounds.maxY))))
         
-        let timingFunction
-            = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        let timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
         
         let positionAnimation = CABasicAnimation()
         positionAnimation.fromValue = NSValue(point: center)
@@ -154,9 +155,8 @@ class ViewController: NSViewController {
         
         let layer = CALayer()
         layer.contents = image
-        layer.actions =
-            ["position" : positionAnimation,
-             "bounds"   : boundsAnimation]
+        layer.actions = ["position" : positionAnimation,
+                         "bounds"   : boundsAnimation]
         
         CATransaction.begin()
         view.layer!.addSublayer(layer)
